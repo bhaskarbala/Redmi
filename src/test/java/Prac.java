6d8acbbcfaece5d77bced1bce38fdf96942dd08e
bhaskarbala <bhaskar3718@gmail.com>
2019-06-02 18:30:29 +0530

    new added

--- src/test/java/Prac.java
+++ src/test/java/Prac.java
@@ -1,12 +1,10 @@
-import com.util.Lib.redmi.DataHandler;
+
 
 public class Prac {
 
 	public static void main(String[] args) throws Throwable {
 		// TODO Auto-generated method stub
-		DataHandler data=new DataHandler();
-		String da=data.getDataForExcelFile("MyTestCase", 15, 1);
-		System.out.println(da);
+		
 		
 		
 
