06cc9c9d054870f95ba036423ea0e1932a312eae
bhaskarbala <bhaskar3718@gmail.com>
2019-06-05 20:10:09 +0530

    Listerner implemation class should tha class we take care of ShreenShot

---
+++ src/main/java/com/util/Lib/redmi/ListernerImp.java
@@ -0,0 +1,65 @@
+package com.util.Lib.redmi;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.IClass;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
+
+public class ListernerImp implements ITestListener{
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		String str=result.getMethod().getMethodName();
+		EventFiringWebDriver edriver=new EventFiringWebDriver(Setup.driver);
+		File src=edriver.getScreenshotAs(OutputType.FILE);
+		File dst=new File("./ScreenShot/"+str+".png");
+		try {
+			FileUtils.copyFile(src, dst);
+		} catch (IOException e) {
+		
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
--- src/test/java/Prac.java
+++ src/test/java/Prac.java
@@ -1,11 +1,14 @@
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.util.Lib.redmi.Setup;
-
+@Listeners(com.util.Lib.redmi.ListernerImp.class)
 public class Prac extends Setup {
 @Test
 	public static void loginmethods() {
 		System.out.println("TestCAsePAssed");
+		System.out.println(20/0);
+		
 	}
 
 }
