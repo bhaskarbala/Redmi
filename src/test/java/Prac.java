d9459ea02b12b4df8b94edec1c3fc4e8157c5639
bhaskarbala <bhaskar3718@gmail.com>
2019-06-18 19:07:53 +0530

    Last update of the methods

--- src/test/java/Prac.java
+++ src/test/java/Prac.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+
+import javax.swing.event.TreeWillExpandListener;
+
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Reporter;
 import org.testng.annotations.Listeners;
@@ -10,7 +14,7 @@ import com.util.Lib.redmi.Setup;
 @Listeners(com.util.Lib.redmi.ListernerImp.class)
 public class Prac extends Setup {
 @Test
-	public static void loginmethods() {
+	public static void loginmethods() throws InterruptedException, Exception {
 	Reporter.log("Navigate to Proudct modulder");
 	Homepage hp=PageFactory.initElements(driver, Homepage.class);
 	hp.getProductsLink().click();
@@ -20,6 +24,11 @@ public class Prac extends Setup {
 	Reporter.log("Nagivate to CreatingNewProudct Page");
 	CreatingNewProduct cr=PageFactory.initElements(driver, CreatingNewProduct.class);
 	cr.getChooseFile().click();
+	
+	Thread.sleep(5000);
+	ProcessBuilder pb=new ProcessBuilder("‪C:\\Users\\NBhaskar\\Desktop\\AutoIt\\new\\very.exe");
+	pb.start();
+	Thread.sleep(1000);
 	
 	
 	
